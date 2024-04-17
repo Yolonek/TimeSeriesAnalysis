7a6aa7305bee4ca5b52706c624d9e949a8dafb70
Yolonek <thejgrabowski@gmail.com>
2024-04-17 22:51:47 +0200

    AR model ready

--- CommonFunctions.py
+++ CommonFunctions.py
@@ -1,4 +1,5 @@
 import mplcyberpunk
+from matplotlib.collections import PolyCollection
 
 
 def enhance_plot(figure, axes, glow=False, alpha_gradient=0, lines=True):
@@ -11,3 +12,9 @@ def enhance_plot(figure, axes, glow=False, alpha_gradient=0, lines=True):
             mplcyberpunk.make_scatter_glow(ax=axes)
     if 1 > alpha_gradient > 0:
         mplcyberpunk.add_gradient_fill(ax=axes, alpha_gradientglow=alpha_gradient)
+
+
+def set_polygon_color(axes, color):
+    for item in axes.collections:
+        if isinstance(item, PolyCollection):
+            item.set_facecolor(color)
